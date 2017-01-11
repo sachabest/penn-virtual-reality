c7b17e8a3e315c0e7a715f99e45f569c9d117f2e
Sacha Best <sachabest@outlook.com>
2017-01-11 16:02:15 -0500

    Add lectures section

--- public/ts/lectures.controller.js
+++ public/ts/lectures.controller.js
@@ -8,20 +8,30 @@ var cis568;
                     {
                         "date": '01/12/17',
                         "topic": 'Introduction and Demos',
-                        "recording": 'none',
-                        "assignments": ''
+                        "recording": 'https://www.youtube.com/watch?v=bY4wAlCicf8',
+                        "assignments": [
+                            {
+                                "name": 'Survey',
+                                "link": 'foo'
+                            }
+                        ]
                     },
                     {
                         "date": '01/17/17',
                         "topic": 'Graphics in 90 Minutes',
-                        "recording": 'none',
-                        "assignments": ''
+                        "recording": 'https://www.youtube.com/watch?v=MNQFxZWHWVA',
+                        "assignments": [
+                            {
+                                "name": 'Graphics Handwritten',
+                                "link": 'foo'
+                            }
+                        ]
                     },
                     {
                         "date": '01/24/17',
                         "topic": 'Three.js basics and OpenGL',
                         "recording": 'none',
-                        "assignments": ''
+                        "assignments": []
                     }
                 ];
             }
--- public/ts/lectures.controller.ts
+++ public/ts/lectures.controller.ts
@@ -9,20 +9,30 @@ namespace cis568.main {
             {
                 "date": '01/12/17',
                 "topic": 'Introduction and Demos',
-                "recording": 'none',
-                "assignments": ''
+                "recording": 'https://www.youtube.com/watch?v=bY4wAlCicf8',
+                "assignments": [
+                    {
+                        "name": 'Survey',
+                        "link": 'foo'
+                    }
+                ]
             },
             {
                 "date": '01/17/17',
                 "topic": 'Graphics in 90 Minutes',
-                "recording": 'none',
-                "assignments": ''
+                "recording": 'https://www.youtube.com/watch?v=MNQFxZWHWVA',
+                "assignments": [
+                    {
+                        "name": 'Graphics Handwritten',
+                        "link": 'foo'
+                    }
+                ]
             },
             {
                 "date": '01/24/17',
                 "topic": 'Three.js basics and OpenGL',
                 "recording": 'none',
-                "assignments": ''
+                "assignments": []
             }
         ];
     }
